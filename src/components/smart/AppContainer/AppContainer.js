e4e5831b8a4bf23224112f1ffa661207d7f88c3e
Arfian Adam <arfianadam@yahoo.com>
2016-11-11 18:46:28 +0700

    Added initial files and folders structure

---
+++ src/components/smart/AppContainer/AppContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './AppContainer.scss'
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'AppContainer';
+  }
+  render() {
+    return (
+			<div className={styles.AppContainer}>
+				{/* Your code here */}
+			</div>
+    )
+  }
+}
+
+export default AppContainer;
