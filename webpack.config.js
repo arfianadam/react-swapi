edc0278e9df1dbbc1e879431002df1f4086b7ec3
Arfian Adam <arfianadam@yahoo.com>
2016-12-01 13:35:53 +0700

    added webpack configuration and development script

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack')
+const path = require('path')
+const merge = require('webpack-merge')
+const extractTextPlugin = require('extract-text-webpack-plugin')
+const combineLoaders = require('webpack-combine-loaders')
+const env = require('dotenv').config()
+const validate = require('webpack-validator')
+
+const srcPath = path.join(__dirname, 'src/')
+const publicPath = path.join(__dirname, 'public/')
+
+const main = {
+	entry: {
+		index: srcPath + 'index.js'
+	},
+	output: {
+		path: publicPath,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react','es2015','stage-0'],
+					plugins: ['transform-decorators-legacy','syntax-decorators']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: extractTextPlugin.extract(
+					'style-loader',
+					combineLoaders([{
+						loader: 'css-loader',
+						query: {
+							minimize: true,
+							modules: true,
+							localIdentName: '[name]-[local]-[hash:base64:5]'
+						}
+					},{
+						loader: 'sass-loader'
+					},{
+						loader: 'autoprefixer-loader',
+						query: {
+							browsers: 'last 2 versions'
+						}
+					}])
+				)
+			}
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'React': 'react',
+			'Promise': 'promise-polyfill',
+			'_': 'lodash',
+		}),
+		new extractTextPlugin('style.css', {
+			publicPath: 'assets/css/'
+		})
+	]
+}
+
+const config = main
+
+module.exports = validate(config)
---
+++ webpack.server.config.js
@@ -0,0 +1,83 @@
+const webpack = require('webpack')
+const path = require('path')
+const fs = require('fs')
+const merge = require('webpack-merge')
+const extractTextPlugin = require('extract-text-webpack-plugin')
+const combineLoaders = require('webpack-combine-loaders')
+const env = require('dotenv').config()
+const validate = require('webpack-validator')
+
+const srcPath = path.join(__dirname, 'src/')
+const publicPath = path.join(__dirname, 'public/')
+
+const main = {
+	entry: {
+		index: path.resolve(__dirname, 'server.js'),
+	},
+	output: {
+		filename: 'server.bundle.js',
+		publicPath: publicPath
+	},
+	target: 'node',
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
+		'react-dom/server', 'react/addons',
+	]).reduce(function (ext, mod) {
+		ext[mod] = 'commonjs ' + mod
+		return ext
+	}, {}),
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react','es2015','stage-0'],
+					plugins: ['transform-decorators-legacy','syntax-decorators']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: extractTextPlugin.extract(
+					'style-loader',
+					combineLoaders([{
+						loader: 'css-loader',
+						query: {
+							minimize: true,
+							modules: true,
+							localIdentName: '[name]-[local]-[hash:base64:5]'
+						}
+					},{
+						loader: 'sass-loader'
+					},{
+						loader: 'autoprefixer-loader',
+						query: {
+							browsers: 'last 2 versions'
+						}
+					}])
+				)
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.ProvidePlugin({
+			'React': 'react',
+			'Promise': 'promise-polyfill',
+			'_': 'lodash',
+		}),
+		new extractTextPlugin('style.css')
+	]
+}
+
+const config = main
+
+module.exports = validate(config)
