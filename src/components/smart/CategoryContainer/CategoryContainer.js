1661482b7d354416621608808e01ee3af8730265
Arfian Adam <arfianadam@yahoo.com>
2016-12-04 22:46:15 +0700

    added CategoryContainer component

---
+++ src/components/smart/CategoryContainer/CategoryContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Waypoint from 'react-waypoint'
+import styles from './CategoryContainer.scss'
+
+class CategoryContainer extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className={ styles.CategoryContainer }>
+				{/* Your code here */}
+			</div>
+		)
+	}
+}
+
+export default CategoryContainer
