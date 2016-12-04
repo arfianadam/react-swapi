f4cc805dc404b263924bfd1ef62c1db80a80a34f
Arfian Adam <arfianadam@yahoo.com>
2016-12-04 22:45:46 +0700

    added route for category

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,13 @@ import { Route, IndexRoute } from 'react-router'
 
 import AppContainer from '../components/smart/AppContainer'
 import CategoriesContainer from '../components/smart/CategoriesContainer'
+import CategoryContainer from '../components/smart/CategoryContainer'
 
 module.exports = (
 	<Route>
 		<Route path='/' component={ AppContainer }>
 			<IndexRoute component={ CategoriesContainer }/>
+			<Route path="/categories/:category" component={ CategoryContainer }/>
 		</Route>
 	</Route>
 )
