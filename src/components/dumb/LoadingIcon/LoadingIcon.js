c995b123b1311da1bc66a3a2c936dc7f871eee2b
Arfian Adam <arfianadam@yahoo.com>
2016-12-05 17:07:08 +0700

    created loading component

---
+++ src/components/dumb/LoadingIcon/LoadingIcon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './LoadingIcon.scss'
+
+class LoadingIcon extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className={ styles.LoadingIcon }>
+				{/* Your code here */}
+			</div>
+		)
+	}
+}
+
+export default LoadingIcon
