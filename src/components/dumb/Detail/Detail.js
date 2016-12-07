38823104808f2852e7f862223bfb4e20259eeb08
Arfian Adam <arfianadam@yahoo.com>
2016-12-07 19:16:14 +0700

    category label in detail now is a link to related category

--- src/components/dumb/Detail/Detail.js
+++ src/components/dumb/Detail/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import styles from './Detail.scss'
 
 import '../../../polyfill/capitalizeFirstLetter'
@@ -14,9 +15,11 @@ class Detail extends React.Component {
 				<div className={ styles.header }>
 					<h1>{ this.props.data.name }</h1>
 					<h5>
-						<label>
-							{ this.props.category.capitalizeFirstLetter() }
-						</label>
+						<Link to={ '/categories/' + this.props.category }>
+							<label>
+								{ this.props.category.capitalizeFirstLetter() }
+							</label>
+						</Link>
 					</h5>
 				</div>
 			</div>
